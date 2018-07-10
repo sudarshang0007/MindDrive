d25c3c9389933838af5155ac230e59ac06b06a88
os2arx <os2arx@pl1inpun0427pc.net.plm.eds.com>
2018-07-10 10:54:10 +0530

    getbyid and post committed

--- src/main/java/com/siemens/hackathon/controller/HelloController.java
+++ src/main/java/com/siemens/hackathon/controller/HelloController.java
@@ -1,16 +1,24 @@
 package com.siemens.hackathon.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.siemens.hackathon.entity.MindDriveEntity;
 import com.siemens.hackathon.model.MindDrive;
+import com.siemens.hackathon.repository.MindDriveRepository;
 
 @CrossOrigin
 @RestController
 public class HelloController {
-	
+	@Autowired
+	MindDriveRepository mindDriveRepository;
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/api/getMindDrive")
 	public MindDrive sayHello() {
@@ -20,4 +28,47 @@ public class HelloController {
 	md.setTime("10");
 		return md;
 	}
+	
+	
+	
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/api/getMindDrive/data")
+    public void sayHelloPostData(@RequestParam Map<String,String> mindData) {
+        MindDriveEntity mindDriveEntity = new MindDriveEntity();
+        mindDriveEntity.setEml(mindData.get("eml"));
+        mindDriveEntity.setPid(mindData.get("id"));
+        mindDriveEntity.setTime(mindData.get("time"));
+        mindDriveEntity.setGpsLongitude(mindData.get("kff1005"));
+        mindDriveEntity.setGpsLatitude(mindData.get("kff1006"));
+
+        mindDriveEntity.setSpeed(mindData.get("kff1001"));
+        mindDriveEntity.setGpsBearing(mindData.get("kff1007"));
+        mindDriveEntity.setAirFuelRatio(mindData.get("kff124d"));
+        mindDriveEntity.setRpm(mindData.get("kc"));
+        mindDriveEntity.setMSensor(mindData.get("kff1223"));
+        mindDriveEntity.setAccZAxis(mindData.get("kff1222"));
+        mindDriveEntity.setAccYAxis(mindData.get("kff1221"));
+        mindDriveEntity.setAccXAxis(mindData.get("kff1220"));
+        mindDriveEntity.setEngCoolantTmp(mindData.get("k5"));
+        mindDriveEntity.setTripDistance(mindData.get("kff1204"));
+        mindDriveEntity.setBaroPressure(mindData.get("k33"));
+        mindDriveEntity.setSysVapPressure(mindData.get("k32"));
+        mindDriveEntity.setFuelFlowratePerMinute(mindData.get("kff125a"));
+        mindDriveEntity.setFuleRemaining(mindData.get("kff126b"));
+        mindDriveEntity.setProfileFuelCost(mindData.get("profileFuelCost"));
+        mindDriveEntity.setProfileFuelName(mindData.get("profileFuelName"));
+        
+        mindDriveEntity.setProfileVe(mindData.get("profileVe"));
+        mindDriveEntity.setProfileWeight(mindData.get("profileWeight"));
+
+        mindDriveRepository.save(mindDriveEntity);
+  
+    }
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/api/getMindDrive/data/{pid}")
+    public MindDriveEntity sayHelloGetData( @PathVariable String pid) {
+	    
+ return     mindDriveRepository.findByPidAndTimeStamp(pid);
+  
+    }
 }
---
+++ src/main/java/com/siemens/hackathon/entity/MindDriveEntity.java
@@ -0,0 +1,110 @@
+package com.siemens.hackathon.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+
+@Table(name="MindDriveEntity")
+public class MindDriveEntity {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    String id;
+    
+    @Column(name="eml")
+    String eml;
+    
+    @Column(name="pid")
+    String pid;
+    
+    @Column(name="time")
+    String time;
+    
+    @Column(name="gpsLongitude")
+    String gpsLongitude;
+    
+    @Column(name="gpsLatitude")
+    String gpsLatitude;
+    
+    @Column(name="speed")
+    String speed;
+    
+    @Column(name="airFuelRatio")
+    String airFuelRatio;
+    
+    @Column(name="rpm")
+    String rpm;
+    
+    
+    @Column(name="accZAxis")
+    String accZAxis;
+    
+    
+    @Column(name="accYAxis")
+    String accYAxis;
+    
+    
+    @Column(name="accXAxis")
+    String accXAxis;
+    
+    @Column(name="engCoolantTmp")
+    String engCoolantTmp;
+    
+    @Column(name="tripDistance")
+    String tripDistance;
+    
+    
+    @Column(name="baroPressure")
+    String baroPressure;
+    
+    
+    @Column(name="engLoad")
+    String engLoad;
+    
+    
+    @Column(name="sysVapPressure")
+    String sysVapPressure;
+    
+    @Column(name="fuelFlowratePerMinute")
+    String fuelFlowratePerMinute;
+    
+    @Column(name="fuleRemaining")
+    String fuleRemaining;
+   
+    @Column(name="gpsBearing")
+    String gpsBearing;
+    
+    @Column(name="mSensor")
+    String mSensor;
+    
+    
+    @Column(name="profileFuelName")
+    String profileFuelName;
+    
+    
+    @Column(name="profileFuelType")
+    String profileFuelType;
+    
+    
+    @Column(name="profileFuelCost")
+    String profileFuelCost;
+    
+    
+    @Column(name="profileVe")
+    String profileVe;
+    
+
+    @Column(name="profileWeight")
+    String profileWeight;
+    
+   
+}
